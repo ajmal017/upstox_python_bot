3e8cd075566267f6cab7845ce3ea7211a2dc6165
shashquatch <shashwat.chetan@gmail.com>
2018-07-19 17:03:47 +0530

    Uses TradeBot subclass and fixed improper termination of bot threads

--- manager.py
+++ manager.py
@@ -1,17 +1,18 @@
 import os
-from threading import Thread
+import threading
 import configparser
 from queue import Queue
 from time import sleep
 from datetime import date, datetime, timedelta
 from upstox_api import api
 
-import utils
+import utils 
 from options import Gann
-from moving_avg import EMAOption
+
+MAX_LOGIN_TRIES = 10
 
 
-class Upstox_Manager(Thread):
+class Upstox_Manager:
 
     def __init__(self, config_name):
         if '.ini' not in config_name.lower():
@@ -24,13 +25,16 @@ class Upstox_Manager(Thread):
 
         self.config = configparser.ConfigParser()
         self.config.read(config_name)
+        self.opening, self.cutoff = utils.get_trade_hours(date.today())
 
         self.client = None
         self.bots = []
-        self.worker_queues = {}
+        self.queues = {}
+        self.threads = []
 
         self.subbed_stocks = []
         self.running = False
+        self.lock = threading.Lock()
 
     def create_config_file(self):
         conf = configparser.ConfigParser()
@@ -48,35 +52,30 @@ class Upstox_Manager(Thread):
         if creds['secret'] == '0':
             creds['secret'] = input('Please enter the API secret - ')
 
+        tries = 0
         s = api.Session(creds['key'])
         s.set_redirect_uri('http://127.0.0.1')
         s.set_api_secret(creds['secret'])
-        diff = None
+        while tries < MAX_LOGIN_TRIES:
+            try:
+                self.client = api.Upstox(creds['key'], creds['token'])
+                print('Logged in successfully.')
+                break
+            except Exception as e:
+                print(e.args)
+                if 'Invalid Bearer token' in e.args[0]:
+                    url = s.get_login_url()
+                    print('New token required. Auth url - ')
+                    print(url)
+                    code = input("Please enter the code from the login page - ")
+                    s.set_code(code)
+                    creds['token'] = s.retrieve_access_token()
+            tries += 1
 
-        if creds['last_login'] == '0':
-            diff = timedelta(hours=1)
-        else:
-            now = datetime.now()
-            last = datetime.strptime(creds['last_login'], '%d-%m-%Y %H:%M')
-            diff = now - last
-
-        if creds['token'] == '0' or diff > timedelta(hours=11, minutes=59):
-            url = s.get_login_url()
-            print('Auth url - ')
-            print(url)
-            code = input("Please enter the code from the login page - ")
-            s.set_code(code)
-            creds['token'] = s.retrieve_access_token()
-        else:
-            print('Reusing token - ', creds['token'])
-        try:
-            self.client = api.Upstox(creds['key'], creds['token'])
-            print('Logged in successfully.')
-        except Exception as e:
-            print('ERROR! Unable to start Upstox client.')
-            print(e)
+        if self.client is None:
             return
 
+        print('Loading master contracts')
         try:
             nse_fo = self.client.get_master_contract('nse_fo')
             if nse_fo:
@@ -97,81 +96,66 @@ class Upstox_Manager(Thread):
         self.client.set_on_trade_update(self.trade_handler)
 
         creds['last_login'] = datetime.now().strftime('%d-%m-%Y %H:%M')
-
         with open(self.config_name, 'w') as cf:
             self.config.write(cf)
 
-    def add_worker(self, worker=None):
-        if worker is None:
-            print("Failed to add worker - Invalid arguments")
-            return
-
-        q = Queue()
-        syms = worker.setup(q)
-        if type(syms) is tuple:
-            self.worker_queues[syms] = q
-        else:
-            self.worker_queues[(syms, )] = q
-
-        self.bots.append(worker)
-
     def run(self):
-        try:
-            while 1:
-                if datetime.now() < utils.TRADE_OPEN:
+        print(self.opening)
+        print(self.cutoff)
+        while 1:
+            try:
+                now = datetime.now()
+                if now < self.opening:
                     print('\nWaiting for trade hours to start')
                     sleep(10)
-                elif datetime.now() > utils.TRADE_CUTOFF:
-                    print('\nCutoff time reached. Close all positions')
-                    self.stop()
-                    break
-                elif not self.running:
+                elif not self.running and now > self.opening and now < self.cutoff:
+                    print('\nStarting bots...')
                     self.client.start_websocket(True)
                     self.running = True
-                    for t in self.bots:
-                        t.start()
-                else:
-                    sleep(1)
-        except Exception as e:
-            self.stop()
+                    self.start_bots()
+                elif now > self.cutoff and self.running is True:
+                    print('\nCutoff time reached. Close all positions')
+                    self.stop()
+                elif now > self.cutoff:
+                    tom = datetime.now() + timedelta(days=1)
+                    self.opening, self.cutoff = utils.get_trade_hours(tom.date())
+                    print('\n')
+                    print(self.opening)
+                    print(self.cutoff)
+            except KeyboardInterrupt:
+                self.stop()
+                return
+            except Exception as e:
+                print('\n-------------------')
+                print(e)
 
     def quote_handler(self, message):
         sym = message['instrument'].symbol.lower()
         if not any(inst for inst in self.subbed_stocks if inst.symbol.lower() == sym):
             self.subbed_stocks.append(message['instrument'])
-
+        print('\n%s | %s - LTP - %f' % (message['timestamp'], sym, message['ltp']))
         try:
-            for k, w in self.worker_queues.items():
+            for k, w in self.queues.items():
                 if sym in k:
-                    self.worker_queues[k].put(('q', message))
+                    self.queues[k].put(('q', message))
         except Exception as e:
-            print('No workers for Symbol %s. Unsubscribing instrument.' % sym)
-            try:
-                self.client.unsubscribe(message['instrument'], api.LiveFeedType.LTP)
-            except Exception as e:
-                pass
-            pass
+            print('No workers for Symbol %s.' % sym)
 
     def order_handler(self, message):
         sym = message['instrument'].symbol.lower()
         try:
-            for k, w in self.worker_queues.items():
+            for k, w in self.queues.items():
                 if sym in k:
-                    self.worker_queues[k].put(('o', message))
+                    self.queues[k].put(('o', message))
         except Exception as e:
-            print('No workers for Symbol %s. Unsubscribing instrument.' % sym)
-            try:
-                self.client.unsubscribe(message['instrument'], api.LiveFeedType.LTP)
-            except Exception as e:
-                pass
-            pass
+            print('No workers for Symbol %s.' % sym)
 
     def trade_handler(self, message):
         sym = message['instrument'].symbol.lower()
         try:
-            for k, w in self.worker_queues.items():
+            for k, w in self.queues.items():
                 if sym in k:
-                    self.worker_queues[k].put(('t', message))
+                    self.queues[k].put(('t', message))
         except Exception as e:
             print('No workers for Symbol %s. Unsubscribing instrument.' % sym)
             try:
@@ -180,8 +164,11 @@ class Upstox_Manager(Thread):
                 pass
 
     def stop(self):
-        for w in self.workers:
-            w.stop()
+        self.running = False
+        for bot in self.bots:
+            bot.stop()
+            if bot.isAlive():
+                bot.join()
 
         for stock in self.subbed_stocks:
             try:
@@ -189,21 +176,32 @@ class Upstox_Manager(Thread):
             except Exception as e:
                 pass
         self.subbed_stocks.clear()
+        if self.client.websocket is not None:
+            self.client.websocket.keep_running = False
 
-        self.client.websocket.keep_running = False
+    def start_bots(self):
+        if len(self.bots) < 1:
+            print('No Bots to run!')
+            return
+        for bot in self.bots:
+            q = Queue()
+            syms = bot.setup(q)
+            if type(syms) is tuple:
+                self.queues[syms] = q
+            elif type(syms) is list:
+                self.queues[tuple(syms)] = q
+            else:
+                self.queues[(syms, )] = q
+            bot.start()
+
 
 
 def main():
     m = Upstox_Manager('config.ini')
     m.login_upstox()
-
-    # g = Gann(m.client)
-    # m.add_worker(g)
-
-    e = EMAOption(m.client)
-    m.add_worker(e)
-
-    # m.run()
+    g = Gann(m.client)
+    m.bots.append(g)
+    m.run()
 
 
 if __name__ == '__main__':

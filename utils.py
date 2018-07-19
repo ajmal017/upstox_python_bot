b6a58db4005c0789a704a6f151535b9b87294f71
shashquatch <shashwat.chetan@gmail.com>
2018-07-19 17:06:13 +0530

    Shifted some constants and definitions from other files into utils

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 from datetime import datetime, date
-import os
-import logging
 
 MAX_LOGIN_TRIES = 10
 TRADE_OPEN = datetime.strptime(date.today().strftime('%d-%m-%y') + '-09:16',
@@ -8,7 +6,18 @@ TRADE_OPEN = datetime.strptime(date.today().strftime('%d-%m-%y') + '-09:16',
 TRADE_CUTOFF = datetime.strptime(date.today().strftime('%d-%m-%y') + '-15:15',
                                  '%d-%m-%y-%H:%M')
 
+BUY = 'B'
+SELL = 'S'
+
+NIFTY_OPTION_TEMPLATE = 'nifty' + date.today().strftime('%y%b').lower() + '%d'
+
 
 def round_off(num, div=0.1):
     x = div * round(num / div)
     return float(x)
+
+
+def get_trade_hours(date):
+    o = datetime.strptime(date.strftime('%d-%m-%y') + '-09:16', '%d-%m-%y-%H:%M')
+    c = datetime.strptime(date.strftime('%d-%m-%y') + '-15:15', '%d-%m-%y-%H:%M')
+    return o, c
